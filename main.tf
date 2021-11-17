5dc04e7d34c937b7c35f802b3987a96f123469a0
Israel Romero <israel870730@gmail.com>
2021-11-17 19:01:30 +0000

    Update output en main que llama al modulo de EC2

--- main.tf
+++ main.tf
@@ -1,53 +1,18 @@
 provider "aws" {
   region = "us-east-1"
 }
-resource "aws_security_group" "group_catalogo" {
-  name = var.sg_name
-  dynamic "ingress"{
-    for_each = var.ingress_rules
-    content {
-        from_port = ingress.value.from_port
-        to_port = ingress.value.to_port
-        protocol = ingress.value.protocol
-        cidr_blocks = ingress.value.cidr_blocks
-    }
-  }
-  dynamic "egress"{
-    for_each = var.egress_rules
-    content {
-        from_port = egress.value.from_port
-        to_port = egress.value.to_port
-        protocol = egress.value.protocol
-        cidr_blocks = egress.value.cidr_blocks
-    }
-  }
-    tags = {
-    Name = "group_catalogo" 
-  }
-}
-resource "aws_instance" "ec2_auto" {
-  count = var.instance_count
-  ami           = var.ami_id
+module "crear-ec2" {
+  source = "./modulos/instance"
+  ami_id = var.ami_id
   instance_type = var.instance_type
-  key_name      = var.key_name
   tags = var.tags
-  security_groups = [aws_security_group.group_catalogo.name]
-  provisioner "remote-exec" {
-    connection {
-      type     = "ssh"
-      user     = "ubuntu"
-      private_key = "${file("~/.ssh/id_rsa")}"
-      host     = self.public_ip
-      timeout     = "3m"
-    }
-    inline = ["echo hello"]
-    #inline = ["echo hello","docker run -it -d -p 8080:80 870730/curso-terraform:v1"]
-  }
+  key_name = var.key_name
+  public_key = var.public_key
+  sg_name = var.sg_name
+  ingress_rules = var.ingress_rules
+  instance_count = var.instance_count
+  egress_rules = var.egress_rules
 }
-resource "aws_key_pair" "deployer" {
-  key_name   = "key_catalogo"
-  public_key = "${file(var.public_key)}"
-}
-output "instance_ips" {
-  value = aws_instance.ec2_auto.*.public_ip
+output "module_instance_ip" {
+  value = module.crear-ec2.instance_ips
 }
--- modulos/instance/variables.tf
+++ modulos/instance/variables.tf
@@ -3,26 +3,34 @@ variable "ami_id" {
   description = "Ami Id"
 }
 variable "instance_type" {
+description = "Public key path"
   
 }
 variable "tags" {
+description = "Variable para los tags"
   type = map
 }
 variable "key_name" {
-  
-}
-variable "key_name_public"{
+description = "Llave publica"
 
 }
 variable "public_key"{
+description = "Public key path"
 
 }
 variable "sg_name" {
+description = "Grupo de sguridad"
 
 }
 variable "ingress_rules" {
+description = "Ingress"
 
 }
 variable "instance_count" {
+description = "Cantidad de intancias"
+
+}
+variable "egress_rules" {
+description = "Egress"
 
 }
